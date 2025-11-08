dc7c2fde72a13949ab86705c13a494fa65e45841
unknown <Yussufobadimeji@gmail.com>
2025-11-08 06:39:31 +0000

    Some changes

--- sign Up/sign up.js
+++ sign Up/sign up.js
@@ -62,7 +62,8 @@ const signUpButton = () => {
 
 
                             } else {
-
+                                let hisName = tunjiUsersObj.name.split(" ")[0];
+                                alert(`Congratulations Dear ${hisName} You have created an account successfully`)
                                 showMailExist.style.display = 'none'
                                 tunjiUsers.push(tunjiUsersObj)
                                 localStorage.setItem('tunji', JSON.stringify(tunjiUsers))
